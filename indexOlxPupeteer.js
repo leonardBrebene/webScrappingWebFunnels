973a038b3f61ee8142c4e66f8a29bd0f8a5bd49f
leonardBrebene <brebene.leonard@yahoo.ro>
2024-01-04 17:28:48 +0200

    Working pupeteer

--- indexOlxPupeteer.js
+++ indexOlxPupeteer.js
@@ -6,25 +6,36 @@ import getNextNavigationPage from "./getNextNavigationPage.js";
 import getLoadedPage from "./getLoadedPage.js";
 import getPathWithDate from "./getPathWithDate.js";
 import setElementValueOnPage from "./setElementValueOnPage.js";
+import  puppeteer from "puppeteer" ;
 
 
 
 const requestDef = request.defaults({ jar: CookieJar })
 const cockieJar = requestDef.jar
 
-const domWindow = await getLoadedPage("https://www.olx.ro/")
-const linkToLogin = await getNextNavigationPage("https://www.olx.ro/","div[color='white'] a")
-console.log("linkToLogin " + linkToLogin)
 
-const domWindowUser = await getLoadedPage(linkToLogin)
-setElementValueOnPage(domWindowUser,"div input[name='username']","value","brebene.leonard@yahoo.ro")
-setElementValueOnPage(domWindowUser,"div input[name='password']","value","parolaBomba.123")
+const main =async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ["--start-maximized"],
+  });
 
-const anchor = domWindow.document.querySelector("div a[data-cy='post-new-ad-button']");
-anchor.click()
-setTimeout(
-  ()=>setElementValueOnPage(domWindow,"div input[name='username']","value","brebene.leonard@yahoo.ro")
-  , 5000);
+  const page = await browser.newPage();
 
-const fileNameHtml = getPathWithDate()
-fs.writeFileSync(fileNameHtml, domWindow.document.body.innerHTML)
+  await page.goto("https://www.wikipedia.org/", {
+    waitUntil: "networkidle2",
+  });
+
+  await page.waitForTimeout(2000);
+
+  let searchText = await page.waitForXPath('//*[@id="searchInput"]');
+  console.log(searchText)
+
+  
+  const [search] = await page.$x(`//*[@id="search-form"]/fieldset/button`);
+  if (search) {
+    await search.click();
+  }
+}
+main()
