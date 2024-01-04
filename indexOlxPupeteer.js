535beb1a2390d1849f250c552ba88d583b4a1f33
leonardBrebene <brebene.leonard@yahoo.ro>
2024-01-04 16:56:18 +0200

    Added puppeteer

--- indexOlxPupeteer.js
+++ indexOlxPupeteer.js
@@ -12,32 +12,6 @@ import setElementValueOnPage from "./setElementValueOnPage.js";
 const requestDef = request.defaults({ jar: CookieJar })
 const cockieJar = requestDef.jar
 
-// const main = async (parameter) => {
-//     console.log(parameter)
-
-//     const clickFunnelsLink = "https://www.olx.ro/"
-//     const result = await requestDef.get(clickFunnelsLink)
-//     let $ =cheerio.load(result)
-
-//     const html= ".html"
-//     const fileName = "./extracted/" + new Date(Date.now() + 2 * 3600 * 1000).toISOString().replace(':', '-').replace(':', '-').slice(0, 19)
-//     console.log("fileName: "+ fileName)
-
-
-//     const dom = new jsdom(result)
-//     const document = dom.window.document
-//     const window=dom.window
-//     // const fileNameJsonDom = fileName + "jsdom" + html
-//     // fs.writeFileSync(fileNameJsonDom , document.body.innerHTML)
-   
-//     window.location.replace()
-
-//     const contulTau = document.querySelector("div[color='white'] a")
-//     console.log( contulTau.getAttribute("href"))
-//     console.log("cockieJar.get:"+ cockieJar.get) 
-// }
-
-// main("Main was called");
 const domWindow = await getLoadedPage("https://www.olx.ro/")
 const linkToLogin = await getNextNavigationPage("https://www.olx.ro/","div[color='white'] a")
 console.log("linkToLogin " + linkToLogin)
