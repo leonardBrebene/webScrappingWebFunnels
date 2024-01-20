dc12b0053f664cb94b320f29f94ee05e10c4e7e3
Leonard Brebene <74564605+leonardBrebene@users.noreply.github.com>
2024-01-20 02:17:43 +0200

    Added smartretry untill element is displayed

--- indexClickFunnelsPupeteer.js
+++ indexClickFunnelsPupeteer.js
@@ -9,7 +9,8 @@ const main = async (url) => {
     headless: false,
     defaultViewport: null,
     args: ["--start-maximized"],
-    executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+   // executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+    executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
     ignoreDefaultArgs: ["--enable-automation"]
   });
   const page = await browser.newPage();
@@ -25,9 +26,50 @@ const main = async (url) => {
   await page.waitForXPath('//*[@class="ui menu secondary"]');
 
   const hrefFunnelsValues = await getAllUrlsFromFunnels(page)
-  await page.goto(hrefFunnelsValues[0])
-  
-  await page.waitForXPath('//*[@class="funnelStepsTitleExplainerTitle pull-left" and contains(text(), "Funnel Steps")]');
+  for (const url of hrefFunnelsValues) {
+    await openFunnel(url);
+  }
+
+  async function openFunnel(hrefFunnelsValue) {
+    console.log("Went stats")
+    await page.waitForTimeout(3000);
+
+    const statsUrl = hrefFunnelsValue + "/stats"
+    await page.goto(statsUrl)
+    await page.waitForXPath('//*[@class="bar"]');
+    await page.waitForXPath('//*[@class=" funnelstep divided"]//td[2]');
+    console.log("Elements charged")
+
+   async function ceva () {
+      const [cell] = await page.$x(`//*[@class=" funnelstep divided"]//td[2]`);
+      const value = await cell.evaluate(el => el.textContent)
+
+      console.log("value of value is:" + value +"'")
+      if(value.includes("\n") || isNaN(value)){
+        console.log("value from setInterval: " + value )
+        await new Promise(resolve => setTimeout(resolve, 1000))
+      }
+      else{
+        return "5"
+      }
+    };
+   for (let index = 0; index < 5; index++) {
+    console.log("index:"+index)
+    const plm=await ceva()
+    if(plm=="5"){
+      console.log("Am intrat pe break")
+        break
+    }
+   }
+
+    const [cell] = await page.$x(`//*[@class=" funnelstep divided"]//td[2]`);
+    const value = await cell.evaluate(el => el.textContent)
+    console.log("value: " + value )
+
+    await page.waitForTimeout(3000);
+    console.log("Out of stats")
+  }
+
   const hrefStepsFunnelValues = await getAllUrlsFromStepFunnels(page)
   await page.goto(hrefStepsFunnelValues[3])
   await page.waitForTimeout(3000);
